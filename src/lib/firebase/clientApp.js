2423e30566e41c96bb738378d31999cd9a6d1ef7
joedewet36 <joedewet36@gmail.com>
2025-03-25 14:58:50 +0200

    'auth fix

--- src/lib/firebase/clientApp.js
+++ src/lib/firebase/clientApp.js
@@ -5,18 +5,8 @@ import { firebaseConfig } from "./config";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { auth } from "firebase-admin";
-
-
-try
-{
-  const app =
+export const firebaseApp =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-  const authy = getAuth(firebaseApp);
-} catch(error) {
-    console.log('Error:', error)
-}
-export const firebaseApp = app;
-export const auth = authy;
+export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
 export const storage = getStorage(firebaseApp);
