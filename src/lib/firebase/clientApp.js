93e3f3c71b546b39934f4e5e9070dee2e5615c52
joedewet36 <joedewet36@gmail.com>
2025-03-25 14:40:42 +0200

    fix data fetch

--- src/lib/firebase/clientApp.js
+++ src/lib/firebase/clientApp.js
@@ -5,8 +5,18 @@ import { firebaseConfig } from "./config";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-export const firebaseApp =
+import { auth } from "firebase-admin";
+
+
+try
+{
+  const app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-export const auth = getAuth(firebaseApp);
+  const authy = getAuth(firebaseApp);
+} catch(error) {
+    console.log('Error:', error)
+}
+export const firebaseApp = app;
+export const auth = authy;
 export const db = getFirestore(firebaseApp);
 export const storage = getStorage(firebaseApp);
